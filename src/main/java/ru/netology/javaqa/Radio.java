62ea16fe4fec1b517439db543c709119c81e7bf7
Dimmidroll <diman15061997@mail.ru>
2025-02-24 21:06:33 +0500

    HW done

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -1,4 +1,44 @@
 package ru.netology.javaqa;
 
 public class Radio {
+    int currentVolume;
+    int currentStation;
+
+    public void increaseStation() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void reduceStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
 }
--- src/test/java/ru/netology/javaqa/RadioTest.java
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -1,4 +1,89 @@
 package ru.netology.javaqa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class RadioTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 9",         // тест верхней границы
+            "6, 5"
+    })
+    public void shouldIncreaseStation(int expected, int set) {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(set);
+        radio.increaseStation();
+
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 5",
+            "9, 0"          // тест нижней границы
+    })
+    public void shouldReduceStation(int expected, int set) {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(set);
+        radio.reduceStation();
+
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 10",        // тест верхней границы
+            "0, -1"        // тест нижней границы
+    })
+    public void setStationBorderTests(int expected, int set) {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(set);
+
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "51, 50",
+            "100, 100"
+    })
+    public void shouldIncreaseVolume(int expected, int set) {
+        Radio radio = new Radio();
+
+        radio.currentVolume = set;
+        radio.increaseVolume();
+
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "49, 50",
+            "0, 0"
+    })
+    public void shouldReduceVolume(int expected, int set) {
+        Radio radio = new Radio();
+
+        radio.currentVolume = set;
+        radio.reduceVolume();
+
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
